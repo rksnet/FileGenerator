eb1c3b77c82b5dd308cda6335ebccea1bd00a749
contactrohitgupta <contactrohitgupta@yahoo.co.in>
2020-04-16 16:27:02 +0530

    XML update

--- FileGeneratorApp/FrmTestFileGenerator.Designer.cs
+++ FileGeneratorApp/FrmTestFileGenerator.Designer.cs
@@ -46,10 +46,9 @@
             // 
             this.lblOpenFile.AutoSize = true;
             this.lblOpenFile.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOpenFile.Location = new System.Drawing.Point(7, 87);
-            this.lblOpenFile.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblOpenFile.Location = new System.Drawing.Point(9, 107);
             this.lblOpenFile.Name = "lblOpenFile";
-            this.lblOpenFile.Size = new System.Drawing.Size(128, 19);
+            this.lblOpenFile.Size = new System.Drawing.Size(159, 23);
             this.lblOpenFile.TabIndex = 5;
             this.lblOpenFile.Text = "Select XML file*:";
             // 
@@ -57,10 +56,10 @@
             // 
             this.btnOpenFile.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOpenFile.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnOpenFile.Location = new System.Drawing.Point(570, 84);
-            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOpenFile.Location = new System.Drawing.Point(760, 103);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(66, 29);
+            this.btnOpenFile.Size = new System.Drawing.Size(88, 36);
             this.btnOpenFile.TabIndex = 3;
             this.btnOpenFile.Text = "Open";
             this.btnOpenFile.UseVisualStyleBackColor = true;
@@ -69,32 +68,33 @@
             // txtFilePath
             // 
             this.txtFilePath.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFilePath.Location = new System.Drawing.Point(161, 84);
-            this.txtFilePath.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFilePath.Location = new System.Drawing.Point(215, 103);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtFilePath.Name = "txtFilePath";
-            this.txtFilePath.Size = new System.Drawing.Size(389, 26);
+            this.txtFilePath.Size = new System.Drawing.Size(517, 30);
             this.txtFilePath.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.Location = new System.Drawing.Point(11, 2);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(15, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(539, 28);
+            this.button1.Size = new System.Drawing.Size(719, 34);
             this.button1.TabIndex = 6;
             this.button1.Text = "Download Xml configuration file";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button2.Location = new System.Drawing.Point(161, 135);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(215, 166);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 28);
+            this.button2.Size = new System.Drawing.Size(160, 34);
             this.button2.TabIndex = 7;
             this.button2.Text = "Get file";
             this.button2.UseVisualStyleBackColor = true;
@@ -113,9 +113,10 @@
             this.panel1.Controls.Add(this.txtFilePath);
             this.panel1.Controls.Add(this.btnOpenFile);
             this.panel1.Controls.Add(this.lblOpenFile);
-            this.panel1.Location = new System.Drawing.Point(12, 80);
+            this.panel1.Location = new System.Drawing.Point(16, 98);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(742, 274);
+            this.panel1.Size = new System.Drawing.Size(988, 336);
             this.panel1.TabIndex = 8;
             // 
             // panel2
@@ -125,30 +126,31 @@
             this.panel2.Controls.Add(this.label1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 47);
+            this.panel2.Size = new System.Drawing.Size(1067, 57);
             this.panel2.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(286, 8);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(381, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 19);
+            this.label1.Size = new System.Drawing.Size(166, 23);
             this.label1.TabIndex = 6;
             this.label1.Text = "Test file generator";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FrmTestFileGenerator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(90)))), ((int)(((byte)(140)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmTestFileGenerator";
             this.Text = "FrmTestFileGenerator";
             this.panel1.ResumeLayout(false);
